c30f4a14496bd846ab5f30dabe6c21082dc33fc3
Dries Michiels <driesm.michiels@gmail.com>
2023-04-10 12:34:15 +0200

    Use puts over printf when printing a literal string

--- dynamicprefixvici.c
+++ dynamicprefixvici.c
@@ -180,7 +180,7 @@ static void ParseCommandLineArguments(int argc, char** argv, CommandLineArgument
 
 static void Usage()
 {
-    printf("usage: dynamicprefixvici [-h] -n pool_name -p prefix_address [-s pool_size[97]]\n"
+    puts("usage: dynamicprefixvici [-h] -n pool_name -p prefix_address [-s pool_size[97]]\n"
             "-h displays the usage message\n"
             "-n pool_name: sets the pool name to add\n"
             "-p prefix_address: sets the prefix_address to add\n"
